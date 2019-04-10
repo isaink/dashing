5a18d1a9979e309286c4086d2ebc190f54e3c72a
Carina <carinataveras@pursuit.org>
2019-04-10 16:22:42 -0400

    Fixed a bug

--- backend/routes/srvProviders.js
+++ backend/routes/srvProviders.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getAllSrvByPro } = require('../db/queries/srvProviders')
 
-router.get('/:id', getAllSrvByPro)
+router.get("/:id", getAllSrvByPro)
 
 
 module.exports = router; 
