32c934ba1ae2d5913c30b9c1a925fe0540ad41dc
Nielene Thomas <nielenethomas@pursuit.org>
2019-04-10 15:59:55 -0400

    getPortfolioForSingleProvider

---
+++ backend/db/queries/portfolio-Q.js
@@ -0,0 +1,29 @@
+const { db } = require('../connector.js');
+
+
+const getPortfolioForSingleProvider = (req, res, next) => {
+  let providerId = parseInt(req.params.provider_id);
+  db.one('SELECT * FROM portfolio WHERE provider_id = $1', [providerId])
+  .then(portfolio => {
+    res.status(200)
+    .json({
+      status: 'success',
+      message: 'You got the Portfolio for this single Provider.',
+      body: portfolio
+    })
+  }).catch(err => {
+    res.status(400)
+    .json({
+      status: 'error',
+      message: "🤣 Na nana na nah. You didn't get the Portfolio belonging to this Provider!😝 "
+    })
+    console.log(err);
+    next();
+  })
+}
+
+
+module.exports = {
+  getPortfolioForSingleProvider,
+
+}
--- backend/routes/services.js
+++ backend/routes/services.js
@@ -3,9 +3,8 @@ let router = express.Router();
 const db = require('../db/queries/service-Q');
 // let { getAllServices } = require ('../db/queries/provider-Q');
 
-
 router.get('/', db.getAllServices );     // http://localhost:3100/services
-router.get('/:service_id', db.getServiceById );     // http://localhost:3100/services/:id
+router.get('/:service_id', db.getServiceById );     // http://localhost:3100/services/:service_id
 
 
 module.exports = router;
