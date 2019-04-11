0be67feb1be16886e47f0d78917662d94170ea04
Nielene Thomas <nielenethomas@pursuit.org>
2019-04-11 19:31:18 -0400

    combo in progress; about to pull from master

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import ComboBox from './components/HomePage/ComboBox'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <ComboBox />
       </div>
     );
   }
