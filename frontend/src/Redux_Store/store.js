a747da30be990c99a839091fa0447c574eb48853
isaink <inks.minds@gmail.com>
2019-04-11 15:23:27 -0400

    distribution components

--- frontend/src/Redux_Store/store.js
+++ frontend/src/Redux_Store/store.js
@@ -8,7 +8,7 @@ import RootReducer from '../Redux_Reducers/RootReducer.js';
 
 const store = createStore(RootReducer, {}, compose(
     applyMiddleware(thunk, logger),
-    window.devToolsExtension ? window.devToolsExtension() : f => f
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
 
 export default store;
