28f509a63b43c3cc6fb0139ed183602cd70e26cc
baynejd <jonathanbayne@pursuit.org>
2019-04-11 16:59:36 -0400

    started work on dropdown

---
+++ frontend/src/components/HomePage/Dropdown.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Dropdown extends Component {
+  constructor(){
+    super();
+    this.state = {
+      services: []
+    };
+  };
+
+  render() {
+    return(
+      <div>
+          <h1>dropdown</h1>
+      </ div>
+
+    )
+  };
+};
+
+export default Dropdown
