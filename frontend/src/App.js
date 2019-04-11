aad03b1c2c17f5233e405bb3a817c74a983ee051
isaink <inks.minds@gmail.com>
2019-04-11 19:57:39 -0400

    starting css Provider Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Provider from './React_Components/ProvidersPage/ProviderBySvc'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Provider />
       </div>
-    );
+    )
   }
-}
+};
 
 export default App;
