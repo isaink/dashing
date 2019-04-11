9f1520fcf84503dfb8f3d103443d0b386f1cfb16
isaink <inks.minds@gmail.com>
2019-04-11 14:17:45 -0400

    fixing the combineReducer varibles

--- frontend/src/Redux_Store/store.js
+++ frontend/src/Redux_Store/store.js
@@ -1,9 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk'
-import logger from 'redux-logger'
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-import RootReducer from '../Redux_Reducers/RootReducer.js'
+import RootReducer from '../Redux_Reducers/RootReducer.js';
 
-const store = createStore(RootReducer, {}, applyMiddleware(thunk, logger))
+// const store = createStore(RootReducer, {}, applyMiddleware(thunk, logger));
+
+const store = createStore(RootReducer, {}, compose(
+    applyMiddleware(thunk, logger),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
 export default store;
