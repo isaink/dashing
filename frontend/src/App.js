3891aea2b13aa5ee91196e806c445ed19db23ad5
jonelle <jonellebain@pursuit.org>
2019-04-17 15:51:07 -0400

    educational page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
-
 import React, { Component } from "react";
 import "./App.css";
 import { Navbar } from "./components/HomePage/Navbar";
 import { Route, Switch } from "react-router-dom";
-import {Home} from "./components/HomePage/Home";
+
+import { Home } from "./components/HomePage/Home";
 // import HairContainer from "./Redux_Containers/HairContainer";
-import Provider from './React_Components/ProvidersPage/ProviderBySvc'
-import ComboBox from './components/HomePage/ComboBox'
+import { Education } from "./components/HomePage/Education";
+import Provider from "./React_Components/ProvidersPage/ProviderBySvc";
+import ComboBox from "./components/HomePage/ComboBox";
 
 import SingleProviderContainer from "./Redux_Containers/SingleProviderContainer";
 
@@ -14,22 +15,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
         <Navbar />
-
-
         <Switch>
           <Route exact path="/" component={Home} />
           {/* <Route path="/hair" component={HairContainer} /> */}
-          <Route path='/providers' component={Provider}/>
-          <Route path='/provider' component={SingleProviderContainer} />
+          <Route path="/providers" component={Provider} />
+          <Route path="/provider" component={SingleProviderContainer} />
+          <Route path="/education" component={Education} />
         </Switch>
 
         <ComboBox />
-
       </div>
-    )
+    );
   }
-};
+}
 
 export default App;
