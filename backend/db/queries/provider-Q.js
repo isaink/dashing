4980f27c872adfaff299a5ced5911c3afc27f467
baynejd <jonathanbayne@pursuit.org>
2019-04-18 15:08:43 -0400

    fixed and added new queries in provider

--- backend/db/queries/provider-Q.js
+++ backend/db/queries/provider-Q.js
@@ -100,26 +100,52 @@ const deleteProvider = (req, res, next) => {
 };
 
 
-const getSingleProvider = (req, res, next) => {
-  db.one('SELECT providers.id, providers.name AS providerName, array_agg(distinct avatar) AS avatar, email, borough, phone_number, website_link, services_provider.service_id, services_provider.provider_id, services.id, services.name AS servicesName, skills.id, skills.service_id, skills.name AS skillsName FROM providers JOIN services_provider ON services_provider.provider_id = providers.id JOIN services ON services_provider.service_id = services.id JOIN skills ON skills.service_id = services.id WHERE provider_id =  ${id} GROUP BY providers.id, services_provider.service_id, services_provider.provider_id, services.id, skills.id', {
-    id: Number(req.params.id)
-  })
-  .then(info => {
-    res.status(200).json({
-      status: 'success',
-      message: 'Got complete infor for provider',
-      info: info
-    })
-  })
-  .catch(err => next(err));
+// const getSingleProvider = (req, res, next) => {
+//   db.one('SELECT providers.id, providers.name AS providerName, array_agg(distinct avatar) AS avatar, email, borough, phone_number, website_link, services_provider.service_id, services_provider.provider_id, services.id, services.name AS servicesName, skills.id, skills.service_id, skills.name AS skillsName FROM providers JOIN services_provider ON services_provider.provider_id = providers.id JOIN services ON services_provider.service_id = services.id JOIN skills ON skills.service_id = services.id WHERE provider_id =  ${id} GROUP BY providers.id, services_provider.service_id, services_provider.provider_id, services.id, skills.id', {
+//     id: Number(req.params.id)
+//   })
+//   .then(info => {
+//     res.status(200).json({
+//       status: 'success',
+//       message: 'Got complete infor for provider',
+//       info: info
+//     })
+//   })
+//   .catch(err => next(err));
+// };
+
+const getProviderInfo = (req, res, next) => {
+ db.one('SELECT providers.id, providers.name AS providerName, avatar, email, borough, phone_number, website_link FROM providers WHERE providers.id = ${id}', {
+   id: Number(req.params.id)
+ })
+ .then(info => {
+   res.status(200).json({
+     status: 'success',
+     message: 'Got complete info for provider',
+     info: info
+   })
+ })
+ .catch(err => next(err));
 };
 
-
+const getProviderServices = (req, res, next) => {
+ db.any('SELECT services_provider.provider_id, services_provider.service_id, services.name AS servicesName, skills.name AS skillsName FROM providers JOIN services_provider ON services_provider.provider_id = providers.id JOIN services ON services_provider.service_id = services.id JOIN skills ON skills.service_id = services.id WHERE providers.id = ${id}', {
+   id: Number(req.params.id)
+ }).then(info => {
+   res.status(200).json({
+     status: 'success',
+     message: 'Got complete services for provider',
+     info: info
+   })
+ })
+ .catch(err => next(err));
+};
 
 
 module.exports = {
   getAllProviders,
-  getSingleProvider,
+  getProviderInfo,
+  getProviderServices,
   createProvider,
   logoutProvider,
   loginProvider,
