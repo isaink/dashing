36b2fe496003631ea5f19ca8ce0b7601cbd9612e
jonelle <jonellebain@pursuit.org>
2019-04-18 19:45:33 -0400

    new save

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
+import Navbar from './components/NavBars/Navbar'
 import Home from "./components/HomePage/Home";
 import { ProviderProfile } from "./components/providerPro/providerProfile";
 
