b678bcd60dc90f64e8275fb4ba80c14e032054a6
jonelle <jonellebain@pursuit.org>
2019-04-24 12:28:27 -0400

    fixed navbar

--- frontend/src/components/HomePage/Dropdown.js
+++ frontend/src/components/HomePage/Dropdown.js
@@ -1,28 +1,34 @@
 import React, { Component } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './../../Css/Dropdown.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./../../Css/Dropdown.css";
 
 import "../../Css/Dropdown.css";
 
 export const Dropdown = props => {
   const handleBoroChange = event => {
-    let borough = event.target.value
-    props.fetchProBySvcAndBoro(borough)
+    let borough = event.target.value;
+    props.fetchProBySvcAndBoro(borough);
   };
 
   return (
     <>
-    <div >
-    <select  className='selector' onChange={handleBoroChange}  style={{width:'300px',  height: '50px'}}  name="select-profession" id="select-profession">
-        <option value=""> Select Your Location </option>
-        <option value="Brooklyn">Brooklyn</option>
-        <option value="Bronx">Bronx</option>
-        <option value="New York">Manhattan</option>
-        <option value="Queens">Queens</option>
-        <option value="Staten Island">Staten Island</option>
-      </select>
-    </div>
-    <hr/>
+      <div>
+        <select
+          className="selector"
+          onChange={handleBoroChange}
+          style={{ width: "300px", height: "50px" }}
+          name="select-profession"
+          id="select-profession"
+        >
+          <option value=""> Select Your Location </option>
+          <option value="Brooklyn">Brooklyn</option>
+          <option value="Bronx">Bronx</option>
+          <option value="New York">Manhattan</option>
+          <option value="Queens">Queens</option>
+          <option value="Staten Island">Staten Island</option>
+        </select>
+      </div>
+      <hr />
     </>
   );
 };
