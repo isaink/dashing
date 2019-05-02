8f64534f896917c24f2c758358710f611311c55e
Nielene Thomas <nielenethomas@pursuit.org>
2019-05-02 07:58:15 -0400

    in progress

--- frontend/src/components/ProfilePage/providerProfile.js
+++ frontend/src/components/ProfilePage/providerProfile.js
@@ -32,214 +32,217 @@ const ProviderProfile = props => {
               alt="intro_picture"
               src={pared}
               className="intro_pic"
-            />
-          {/*
-            style={{ width: `100%` }}
-            */}
-          </div>
-
-          <div className='allBelowBkGndImage'>
-
-            <div className='backLinkDiv'>
-              {/*
-              <span className="dashingNav">
-                <h3>
-                  <Link to={"/"}>
-                    <img
-                      src={logo}
-                      alt=""
-                      width="175px"
-                      height="auto"
-                    />
-                  </Link>
-                </h3>
-              </span>
+              />
+            {/*
+              style={{ width: `100%` }}
               */}
+            </div>
 
-              <Link
-                className="backLink"
-                to={"/"}
-                onClick={goBack}
-                style={{textDecoration: 'none'}}
+            <div className='allBelowBkGndImage'>
+
+              <div className='backLinkDiv'>
+                {/*
+                  <span className="dashingNav">
+                  <h3>
+                  <Link to={"/"}>
+                  <img
+                  src={logo}
+                  alt=""
+                  width="175px"
+                  height="auto"
+                  />
+                </Link>
+              </h3>
+            </span>
+            */}
+
+            <Link
+              className="backLink"
+              to={"/"}
+              onClick={goBack}
+              style={{textDecoration: 'none'}}
               >
 
-                <div className='backarrowImageTagDiv'>
-                  <img src={backarrow}
-                    alt=''
-                    width='50px'
-                    className='backarrowImageTag'
+              <div className='backarrowImageTagDiv'>
+                <img src={backarrow}
+                  alt=''
+                  width='50px'
+                  className='backarrowImageTag'
                   />
-                </div>
+              </div>
 
-                <div className='goBackTextTag'>
-                  <h4> Go Back </h4>
-                </div>
+              <div className='goBackTextTag'>
+                <h4> Go Back </h4>
+              </div>
 
-              </Link>
+            </Link>
 
-            </div>
+          </div>
 
-            <div className='leftAndRightSidesDiv'>
-              <div className='AvatarAndAllDetailsDiv'>
+          <div className='leftAndRightSidesDiv'>
+            <div className='AvatarAndAllDetailsDiv'>
 
-                <div className='avatarProfileDiv'>
-                  <div className='avatarProfileBorder'>
-                    <img
-                      src={props.providerInfo.avatar}
-                      alt="avatar"
-                      className="avatarProfile"
+              <div className='avatarProfileDiv'>
+                <div className='avatarProfileBorder'>
+                  <img
+                    src={props.providerInfo.avatar}
+                    alt="avatar"
+                    className="avatarProfileImage"
                     />
-                  </div>
-
-                  <div className="prov_info">
-                    <h5 className="prov_name">{props.providerInfo.providername} </h5>
-                  </div>
                 </div>
 
+                <div className="prov_info">
+                  <h5 className="prov_name">{props.providerInfo.providername} </h5>
+                </div>
+              </div>
 
 
-                {/* <div className="bg_name" />   ???????   */}
 
-                {/* <div className='ctnr_contact'> */}
+              {/* <div className="bg_name" />   ???????   */}
 
-                <div className='contactLocationSocialLinkDiv'>
-                  <div className="phoneEmailAndSocialLinkDiv">
-                    <div className='phoneNumDiv'>
-                      <p>Phone number: </p>
-                      <h6>{props.providerInfo.phone_number}</h6>
-                    </div>
-                    <div className='emailDiv'>
-                      <p>Email:</p>
-                      <h6>{props.providerInfo.email}</h6>
-                    </div>
-                    {/* </div> */}
+              {/* <div className='ctnr_contact'> */}
 
-                    <div className="social_link">
-                      <p>Social media: </p>
-                      <a href="https://www.instagram.com/krystamariehair/"
-                        target="_blank" rel='noopener noreferrer'>
-                        <h6>{props.providerInfo.website_link}</h6>
-                      </a>
-                    </div>
-
+              <div className='contactLocationSocialLinkDiv'>
+                <div className="phoneEmailAndSocialLinkDiv">
+                  <div className='phoneNumDiv'>
+                    <p>Phone number: </p>
+                    <h6>{props.providerInfo.phone_number}</h6>
                   </div>
+                  <div className='emailDiv'>
+                    <p>Email:</p>
+                    <h6>{props.providerInfo.email}</h6>
+                  </div>
+                  {/* </div> */}
 
-                  <div className='locDiv'>
-                    <div className="loc">
-                      <div className="prov_boro">
-                        <img
-                          alt="loc"
-                          src={loc}
-                          style={{ zIndex: "2", height: "20px" }}
-                          />
-                        <h5>{props.providerInfo.borough}</h5>
-                      </div>
-                    </div>
-
-
+                  <div className="social_link">
+                    <p>Social media: </p>
+                    <a href="https://www.instagram.com/krystamariehair/"
+                      target="_blank" rel='noopener noreferrer'
+                      className='socialLink_a_tag'>
+                      <h6>{props.providerInfo.website_link}</h6>
+                    </a>
                   </div>
 
                 </div>
 
-              </div>
-
-              {/*
-              <div className="ctnr_porfolio">
-                //  <img src={lines}/>
-                //  PORFOLIO HERE
-              </div>
-              */}
-
-            <div className='rightSideDiv'>
-              <div className='portfolioDiv'>
-                <div className="ctnr_portfolio">
-                  {props.providerInfo.portfolio
-                    ? (props.providerInfo.portfolio.map(photo => {
-                      return (
-                        <img
-                          className="portfolio_img"
-                          src={photo["img"]}
-                          alt="not here"
-                          style={{ height: "200px" }}
-                          />
-                        );
-                      }))
-                    : (
-                      <>
+                <div className='locDiv'>
+                  <div className="loc">
+                    <div className="prov_boro">
                       <img
-                        alt="intro_picture"
-                        src={not_img}
-                        style={{
-                          objectFit: "cover",
-                          height: "320px",
-                          transform: "translateY(-20%)"
-                        }}
+                        alt="loc"
+                        src={loc}
+                        style={{ zIndex: "2", height: "30px" }}
                         />
-                      </>
-                    )
-                  }
+                      <h6>{props.providerInfo.borough}</h6>
+                    </div>
+                  </div>
+
+
+                </div>
+
               </div>
+
             </div>
 
-            <div className="ctnr_services">
-              {props.providerInfo.services &&
-                props.providerInfo.services.map(service => {
-                  let skills = service.skills.map(skill => {
-                    return (
-                      <>
-                      <img
-                        alt="intro_picture"
-                        src={bulletpoint}
-                        style={{ height: "5px" }}
-                        />
-                      {' '} {skill} {' '}
-                      </>
-                  );
-                });
-                return (
-                  <>
-                  <div className="ctnr_skills">
-                    <p className="srv_name">{service.servicesname}</p>
-                  </div>
-                  <div className="inner_skill">{skills}</div>
-                  </>
-              );
-            })}
-          </div>
-
-          </div>
-        </div>
-
-
-
-          </div>
             {/*
-            <div className='testDiv'>
-              <h1 className='testTag'> Test Test tEst tEat stea tl la fjkla sd af</h1>
+              <div className="ctnr_porfolio">
+              //  <img src={lines}/>
+              //  PORFOLIO HERE
             </div>
             */}
+            <div className='centerDiv'>
+              <div className="ctnr_services">
+                {props.providerInfo.services &&
+                  props.providerInfo.services.map(service => {
+                    let skills = service.skills.map(skill => {
+                      return (
+                        <>
+                        <img
+                          alt="intro_picture"
+                          src={bulletpoint}
+                          style={{ height: "5px" }}
+                          className='skillBullets'
+                          />
+                        {' '} <p className='skill_item'>{skill}</p> {' '}
+                        </>
+                    );
+                  });
+                  return (
+                    <>
+                    <div className="srv_nameDiv">
+                      <p className="srv_name">{service.servicesname}</p>
+                    </div>
+                    <div className="skillAndBulletsDiv">{skills}</div>
+                    </>
+                );
+              })}
+            </div>
 
+          </div>
+
+          <div className='rightSideDiv'>
+            <div className='portfolioDiv'>
+              <div className="ctnr_portfolio">
+                {props.providerInfo.portfolio
+                  ? (props.providerInfo.portfolio.map(photo => {
+                    return (
+                      <img
+                        className="portfolio_img"
+                        src={photo["img"]}
+                        alt="not here"
+                        style={{ height: "200px" }}
+                        />
+                    );
+                  }))
+                  : (
+                    <>
+                    <img
+                      alt="intro_picture"
+                      src={not_img}
+                      style={{
+                        objectFit: "cover",
+                        height: "320px",
+                        transform: "translateY(-20%)"
+                      }}
+                      />
+                    </>
+                )
+              }
+            </div>
+          </div>
         </div>
-      );
 
-    } else {
-      return (
-        <div key="1" id="providerInfo">
-          Collecting Info ...{" "}
-        </div>
-      );
-    }
-  };
+      </div>
 
 
-  console.log(  props.providerInfo.services);
-  console.log(  props.providerInfo.portfolio);
-  console.log(props.providerInfo.providername);
-  return (
-    <div>
-      {renderProviderInfo()}
     </div>
-  )
+    {/*
+      <div className='testDiv'>
+      <h1 className='testTag'> Test Test tEst tEat stea tl la fjkla sd af</h1>
+    </div>
+    */}
+
+  </div>
+);
+
+} else {
+  return (
+    <div key="1" id="providerInfo">
+      Collecting Info ...{" "}
+    </div>
+  );
+}
+};
+
+
+console.log(  props.providerInfo.services);
+console.log(  props.providerInfo.portfolio);
+console.log(props.providerInfo.providername);
+return (
+  <div>
+    {renderProviderInfo()}
+  </div>
+)
 };
 
 export default withRouter(ProviderProfile)
