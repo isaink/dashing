b355f8ee13648c8bd0ffdc73242139f6bf934203
jonelle <jonellebain@pursuit.org>
2019-05-08 11:38:51 -0400

    commiting deployment

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'frontend/build')));
 //  app.get('*', (req, res) => {
 //    res.sendfile(path.join(__dirname = 'frontend/build/index.html'));
 //  })
-}
+// }
 //build mode
 
 
