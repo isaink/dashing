222e95fdda41998a6b1752524728fdc8bed52674
jonelle <jonellebain@pursuit.org>
2019-05-20 10:40:09 -0400

    commit

--- backend/frontend/src/Redux_Store/store.js
+++ backend/frontend/src/Redux_Store/store.js
@@ -1,4 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+// import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
