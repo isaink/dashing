10a009891f388761101be0bd98fb04c05611e9d5
jonelle <jonellebain@pursuit.org>
2019-05-20 10:47:35 -0400

    commit

--- backend/frontend/src/Redux_Store/store.js
+++ backend/frontend/src/Redux_Store/store.js
@@ -1,16 +1,21 @@
-// import { createStore, applyMiddleware, compose } from 'redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+// import { createStore, applyMiddleware } from 'redux';
 
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
 import RootReducer from '../Redux_Reducers/RootReducer.js';
 
-const store = createStore(RootReducer, {}, applyMiddleware(thunk, logger));
+// const store = createStore(RootReducer, {}, applyMiddleware(thunk, logger));
 
 // const store = createStore(RootReducer, {}, compose(
 //     applyMiddleware(thunk, logger),
 //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(RootReducer, {}, compose(
+    applyMiddleware(thunk, logger),
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 ));
+
 
 export default store;
